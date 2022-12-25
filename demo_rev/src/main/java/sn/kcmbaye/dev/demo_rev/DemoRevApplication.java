6769c3b62015a2843130e5499c367ea469abaf9e
Mbaye Kasse <Kcmbaye4@gmail.com>
2022-12-25 15:13:02 +0000

    Ajout du nouveau projet demo_rev dans le repository ousmane_project

---
+++ demo_rev/src/main/java/sn/kcmbaye/dev/demo_rev/DemoRevApplication.java
@@ -0,0 +1,13 @@
+package sn.kcmbaye.dev.demo_rev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRevApplication.class, args);
+	}
+
+}
---
+++ demo_rev/src/test/java/sn/kcmbaye/dev/demo_rev/DemoRevApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.kcmbaye.dev.demo_rev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
